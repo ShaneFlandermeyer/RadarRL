9bcaa995962203824e3894febffa2e64140539b9
shane <shaneflandermeyer@gmail.com>
2022-04-30 10:50:41 -0500

    Added target state back into MDPRadar

--- MDPRadar.py
+++ MDPRadar.py
@@ -67,13 +67,14 @@ channel = np.zeros((N,))
 Theta = np.array(list(itertools.product([0, 1], repeat=N)))
 
 # Interfering system
-interference_state = 1
+# interference_state = 1
 # comms = HoppingInterference(pattern=np.array(
 #     [2**n for n in itertools.chain(range(N), range(N-2, 0, -1))]),
 #     tx_power=4.6e-13, states=Theta)
-seq_length = 50
-comms = HoppingInterference(pattern=np.random.randint(
-    0, Theta.shape[0]-1, seq_length), tx_power=4.6e-13, states=Theta)
+seq_length = 10
+comms = HoppingInterference(pattern=2**(np.random.randint(
+    0, N+1, seq_length)), tx_power=4.6e-13, states=Theta)
+comms.pattern[comms.pattern == 2**N] -= 1
 
 # %% [markdown]
 # ## Simulation Environment
@@ -95,8 +96,8 @@ for i in range(N):
     actions = np.append(actions, np.array([np.roll(state, j) for j in
                                            range(N-i)]), axis=0)
 # Number of position states in each dimension
-rho_x = 5
-rho_y = 5
+rho_x = 2
+rho_y = 2
 rho_z = 1
 rho = rho_x*rho_y*rho_z
 rx = np.linspace(-5e3, 5e3, rho_x)
@@ -104,8 +105,8 @@ ry = np.linspace(1e3, 5e3, rho_y)
 rz = np.linspace(0, 0, rho_z)
 P = np.vstack(np.meshgrid(rx, ry, rz)).reshape(3, -1).T
 # Number of velocity states
-nu_x = 3
-nu_y = 3
+nu_x = 2
+nu_y = 2
 nu_z = 1
 nu = nu_x*nu_y*nu_z
 # NOTE: The min/max velocity I have set in each dimension is completely arbitrary
@@ -113,7 +114,7 @@ vx = np.linspace(-5, 5, nu_x)
 vy = np.linspace(-5, 5, nu_y)
 vz = np.linspace(0, 0, nu_z)
 V = np.vstack(np.meshgrid(vx, vy, vz)).reshape(3, -1).T
-target = Target(position=[], velocity=[], rcs=0.1)
+target = Target(position=[], velocity=[], rcs=0.1, P=P, V=V)
 
 
 # %%
@@ -144,7 +145,7 @@ def reward(radar_state, interference_state):
     # interference exists but the radar did not transmit there
     num_missed_opportunity = np.sum(
         (radar_state == 0) & (interference_state == 0))
-    r += -100*num_collision
+    r += -45*num_collision
     r += 10*(num_subband-1)
     return r
 
@@ -191,9 +192,17 @@ def animate_spectrum(tx_history, interference_history):
         fig, animate, tx_history.shape[0], repeat=False, blit=True)
     anim.save('./test.gif', fps=1)
 
+# %%
+
+
+def compute_state():
+    state_ind = comms.state_ind*rho*nu + \
+        target.vel_state_ind*rho + target.pos_state_ind
+    return state_ind
 
 # %% [markdown]
 # ## Train the MDP
+
 
 # %%
 # Number of possible states (here, equal to the number of interference states)
@@ -202,6 +211,7 @@ if use_memory:
     S = 2**(2*N)
 else:
     S = 2**N
+S *= rho*nu
 # Number of possible actions
 A = actions.shape[0]
 # Initialize the transition and reward matrices
@@ -210,7 +220,6 @@ R = np.zeros((A, S, S))
 
 num_train = int(1e3)
 num_test = int(1e2)
-# time = np.linspace(0, 1500, max(20, comms.pattern.shape[0]))
 time = np.linspace(0, 1500, 20)
 # Time step for the simulation
 dt = time[1] - time[0]
@@ -219,34 +228,37 @@ for itrain in range(num_train):
     target.position = P[np.random.choice(P.shape[0]), :]
     target.velocity = V[np.random.choice(V.shape[0]), :]
     # Add a gaussian perturbance to the position and velocity
-    target.position += np.random.randn()
+    target.position[:-1] += np.random.randn(2)
     target.velocity += np.random.randn()
-    target.position[-1] = max(target.position[-1], 0)
+    target.pos_state_ind = np.linalg.norm(
+        target.position - P, keepdims=True, axis=1).argmin()
+    target.vel_state_ind = np.linalg.norm(
+        target.velocity - V, keepdims=True, axis=1).argmin()
     # Start at a random place in the pattern
     comms.pattern_ind = np.random.randint(0, comms.pattern.shape[0])
     comms.state_ind = comms.pattern[comms.pattern_ind]
     comms.current_state = comms.states[comms.state_ind]
-    next_state = (0, comms.state_ind)
+    prev_comms_state = 0
+    next_state = (0, compute_state())
     for t in time:
         # Initial environment state
         current_state = next_state
         # Randomly select a valid action
         action_index = np.random.randint(0, A)
         radar.action = actions[action_index, :]
-
         # Determine the bandwidth used, then update the interference, position, range, and SINR
         wave.bandwidth = subband_bw*np.sum(radar.action)
+        current_comms_state = comms.state_ind
         comms.step()
         target.step(dt)
-        next_state = (current_state[1], comms.state_ind)
-        # We also need the indices of the interference state (with memory) in
-        # the Theta array
+        next_state = (current_state[1], compute_state())
+        # If memory is used
+        current_state_ind = current_state[1]
+        next_state_ind = next_state[1]
         if use_memory:
-            current_state_ind = current_state[0]*(2**N) + current_state[1]
-            next_state_ind = next_state[0]*(2**N) + next_state[1]
-        else:
-            current_state_ind = next_state[0]
-            next_state_ind = next_state[1]
+            current_state_ind += prev_comms_state*(2**N)*rho*nu
+            next_state_ind += current_comms_state*(2**N)*rho*nu
+        prev_comms_state = current_comms_state
         T[action_index, current_state_ind, next_state_ind] += 1
         R[action_index, current_state_ind,
             next_state_ind] += reward(radar.action, comms.current_state)
@@ -276,31 +288,35 @@ for itest in range(num_test):
     # Randomly select a starting position and target velocity
     target.position = np.random.rand(3)*(np.max(P)-np.min(P)) + np.min(P)
     target.velocity = np.random.rand()*(np.max(V)-np.min(V)) + np.min(V)
-
+    target.pos_state_ind = np.linalg.norm(
+        target.position - P, keepdims=True, axis=1).argmin()
+    target.vel_state_ind = np.linalg.norm(
+        target.velocity - V, keepdims=True, axis=1).argmin()
     tx_history = np.empty((0, N))
     interference_history = np.empty((0, N))
     comms.pattern_ind = np.random.randint(0, comms.pattern.shape[0])
     comms.state_ind = comms.pattern[comms.pattern_ind]
     comms.current_state = comms.states[comms.state_ind]
-    next_state = (0, comms.state_ind)
+    next_state = (0, compute_state())
+    prev_comms_state = 0
     for itime in range(time.shape[0]):
         # Initial environment state
         current_state = next_state
+        current_state_ind = current_state[1]
         if use_memory:
-            current_state_ind = current_state[0]*(2**N) + current_state[1]
-        else:
-            current_state_ind = current_state[1]
+            current_state_ind += prev_comms_state*(2**N)*rho*nu
         # Select an action from the policy
         radar.action = actions[pi.policy[current_state_ind], :]
 
         # Determine the bandwidth used, then update the interference, position, range, and SINR
         wave.bandwidth = subband_bw*np.sum(radar.action)
+        prev_comms_state = comms.state_ind
         comms.step()
         target.step(dt)
         if itime > 0:
             current_reward[itime, itest] = current_reward[itime -
                                                           1, itest] + reward(radar.action, comms.current_state)
-        next_state = (current_state[1], comms.state_ind)
+        next_state = (current_state[1], compute_state())
 
         # Variables for plotting
         interference_history = np.append(interference_history, np.reshape(
--- target.py
+++ target.py
@@ -1,9 +1,10 @@
 import numpy as np
 
+
 class Target:
     """Point target object"""
 
-    def __init__(self, position, velocity, rcs):
+    def __init__(self, position, velocity, rcs, P=np.array([]), V=np.array([])):
         """Instantiate a target object
 
         Args:
@@ -16,7 +17,17 @@ class Target:
         self.position = np.array(position)
         self.velocity = np.array(velocity)
         self.rcs = rcs
-        
+        self.P = P
+        self.V = V
+        self.pos_state_ind = -1
+        self.vel_state_ind = -1
+        if P.size > 0 and self.position.size > 0:
+            self.pos_state_ind = np.linalg.norm(
+                self.position - P, keepdims=True, axis=1).argmin()
+        if V.size > 0 and self.velocity.size > 0:
+            self.vel_state_ind = np.linalg.norm(
+                self.velocity - V, keepdims=True, axis=1).argmin()
+
     def step(self, dt):
         """Update the target motion profile
 
@@ -24,3 +35,9 @@ class Target:
             dt (float): Time change since last step
         """
         self.position += self.velocity*dt
+        if self.P.size > 0:
+            self.pos_state_ind = np.linalg.norm(
+                self.position - self.P, keepdims=True, axis=1).argmin()
+        if self.V.size > 0:
+            self.vel_state_ind = np.linalg.norm(
+                self.velocity - self.V, keepdims=True, axis=1).argmin()
